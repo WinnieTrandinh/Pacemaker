569f29a8dfa70d05686eb097353ace8826cb0996
Winnie <22330162+WinnieTrandinh@users.noreply.github.com>
2020-10-26 23:40:07 -0400

    Added new classes for buttons, labels, entries, and windows.

--- DCM/3K04 DCM/completeInterface.py
+++ DCM/3K04 DCM/completeInterface.py
@@ -14,6 +14,65 @@ with open("pacemaker_passwords.txt") as i:
     password_list = i.readlines()
     password_list = [x.strip() for x in password_list]
 
+# *****************************************  new classes ***************************************************
+# parent class of all gComp (graphical component) classes, excluding the label
+# also happens to be a button
+class Button(tk.Tk):
+    def __init__(self, window, textLabel, function):
+        self.gComp = tk.Button(window, text=textLabel, font=("Comic Sans MS", 15), command=function)
+
+    def place(self, xPos, yPos, width, height):
+        self.gComp.place(x=xPos, y=yPos, width=width, height=height)
+
+# label class
+class Label(tk.Tk):
+    def __init__(self, canvas, textLabel):
+        self.gComp = tk.Label(canvas, text=textLabel, bg='#FFB6C1', font=('Comic Sans MS', 30))
+
+    def place(self, xPos, yPos, width):
+        self.gComp.place(x=xPos, y=yPos, width=width)
+        self.gComp.bind('<Button-1>', self.hide)
+
+    # call this to hide a label
+    # to reshow a label, call place again
+    def hide(self):
+        self.gComp.place_forget()
+
+# entry component inherits from Button
+class Entry(Button):
+    # different constructor needed though
+    def __init__(self, canvas):
+        self.gComp = tk.Entry(canvas, font=("Comic Sans MS", 20));
+    # kind of stupid to have a wrapper function with the same name,
+    # but I'm too lazy to change all the get() calls in the code
+    # Probably better to do that though, and change this function
+    # into getEntry() or something.
+    def get(self):
+        return self.gComp.get()
+
+# same as above but inputs show as *
+class PasswordEntry(Entry):
+    def __init__(self, canvas):
+        self.gComp = tk.Entry(canvas, font=("Comic Sans MS", 20), show="*")
+
+# class for window creation
+class Window(tk.Tk):
+    def __init__(self, canvas, x, y, height, width, title):
+        self.window = tk.Toplevel(canvas, height=height, width=width, bg='#FFB6C1')
+        self.setPos(x, y)
+        self.setTitle(title)
+    # this sets the position of the window when created
+    def setPos(self, x, y):
+        self.window.geometry("+%d+%d" % (x, y))
+
+    def setTitle(self, title):
+        self.window.title(title)
+    # returns the actual window object so that it can be used as a canvas for gComponents
+    def getWindow(self):
+        return self.window
+
+# ****************************************************************************************************
+
 
 root = tk.Tk()
 root.title('Login/Signup')
@@ -29,21 +88,33 @@ background_image = tk.PhotoImage(file='Heart.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
+# ************************* label demo ************************************
+fooLabel = Label(root, "foo")
+fooLabel.place(100, 50, 200)
+fooLabel.hide()
+# *************************************************************************
+
 welcomeLabel = tk.Label(root, text = "Welcome!", bg = '#FFB6C1', font = ("Comic Sans MS", 30))
 
 welcomeLabel.place(x= 300, y= 100, width = 200,)
 
 nameLabel = tk.Label(root,text="Enter Name:", bg='#FFB6C1',font = ("Comic Sans MS", 20)).place(x=150,y=260)
 
-nameEntry = tk.Entry(root, font=("Comic Sans MS", 20))
-nameEntry.place(x = 150, y = 300, width = 500, height = 50)
-
+# ******************* Entry demo ******************************************
+#nameEntry = tk.Entry(root, font=("Comic Sans MS", 20))
+#nameEntry.place(x = 150, y = 300, width = 500, height = 50)
+nameEntry = Entry(root)
+nameEntry.place(150, 300, 500, 50)
+# *************************************************************************
 
 passwordLabel = tk.Label(root,text="Enter Password:", bg='#FFB6C1',font = ("Comic Sans MS", 20)).place(x=150,y=460)
 
-
-passwordEntry = tk.Entry(root, font=("Comic Sans MS", 20), show = "*")
-passwordEntry.place(x = 150, y = 500, width = 500, height = 50)
+# *************************** Password demo *******************************
+#passwordEntry = tk.Entry(root, font=("Comic Sans MS", 20), show = "*")
+#passwordEntry.place(x = 150, y = 500, width = 500, height = 50)
+passwordEntry = PasswordEntry(root)
+passwordEntry.place(150, 500, 500, 50)
+# *************************************************************************
 
 def quit(window):
         window.destroy()
@@ -55,21 +126,29 @@ def chooseDisplay(username, password):
     
     for i in range(len(user_list)):  #loops through all names within the user list 
         if(username == user_list[i] and password == password_list[i]):
-            chooseDispWindow = tk.Toplevel(root,  height = HEIGHT, width = WIDTH, bg = '#FFB6C1')
-            chooseDispWindow.title("Choose Display")
+
+            # ************************************************* window demo ***************************************************************
+            #chooseDispWindow = tk.Toplevel(root,  height = HEIGHT, width = WIDTH, bg = '#FFB6C1')
+            #chooseDispWindow.title("Choose Display")
+            chooseDispWindow = Window(root, root.winfo_x(), root.winfo_y(), HEIGHT, WIDTH, "Choose Display").getWindow()
+            # *****************************************************************************************************************************
+
             nameLabel3 = tk.Label(chooseDispWindow,text="What would you like to display?", bg='#FFB6C1',font = ("Comic Sans MS", 20)).place(x=200,y=150)
 
             modeButton = tk.Button(chooseDispWindow, text="Display Pacing Modes", font=("Comic Sans MS", 15),command = lambda:pacingMode())
             modeButton.place(x = 250, y = 250, width = 300, height = 50)
 
-            dataButton = tk.Button(chooseDispWindow, text="Input Data Values", font=("Comic Sans MS", 15),command = lambda:dataValues())
-            dataButton.place(x = 250, y = 350, width = 300, height = 50)
+            # ********************************************* Button demo ***********************************************************************************(*****
+            #dataButton = tk.Button(chooseDispWindow, text="Input Data Values", font=("Comic Sans MS", 15),command = lambda:dataValues())
+            #dataButton.place(x = 250, y = 350, width = 300, height = 50)
+            dataButton = Button(chooseDispWindow, "Input Data Values", lambda: dataValues())
+            dataButton.place(250, 350, 300, 50)
+            # ***************************************************************************************************************************************************
+
         elif(username == user_list[i] and password != password_list[i]):
             incorrectPassLabel = tk.Label(root, text = "Incorrect Password!", bg = '#FFB6C1', font = ("Comic Sans MS", 20)).place(x = 150, y = 550, width = 500, height = 50)
         elif(username != user_list[i]):
             noUserLabel = tk.Label(root, text = "User not found!", bg = '#FFB6C1', font = ("Comic Sans MS", 20)).place(x = 150, y = 550, width = 500, height = 50)
-
-
 
 def lowerRate():
     lowerRateWindow = tk.Toplevel(root,  height = HEIGHT, width = WIDTH, bg = '#FFB6C1')
