5411112040fb0e4f7ee8e78b01f5878e8b7835c3
Winnie <22330162+WinnieTrandinh@users.noreply.github.com>
2020-11-18 23:42:21 -0500

    Added working serial test model.
    Com port in py script should be connecting to the com port not used by Heartview.

--- Pacemaker/serial_writing.py
+++ Pacemaker/serial_writing.py
@@ -1,12 +1,18 @@
 import serial
-ser=serial.Serial("COM4",115200)
+from struct import *
+ser=serial.Serial("COM4",115200, timeout = 10)
 #ser.port='COM4'
-print(ser.name)
-print(ser.is_open)
-print(ser)
-ser.write([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,])
+print("name: ", ser.name)
+print("is open: ", ser.is_open)
+print("serial: ", ser)
 
+# first byte = 22
+# second byte = 34 for echo, 85 for set param
+print("num bytes: ", ser.write([22, 85, 0,1,1,1,1,1,1,1]))
 
+message = ser.read(10);
+print("message: ", message);
+#print("unpacked: ", unpack('message', b'\x01\x02\x03\x04\x05\x06\x07\x08\t\n') )
 
 
 
