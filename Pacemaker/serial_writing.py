45f1ea7cfa3ca96e18fdbf5187e6fb70ec1067b0
Winnie <22330162+WinnieTrandinh@users.noreply.github.com>
2020-11-27 21:06:58 -0500

    Integrated serial with parameters in model.
    Updated datatypes into smaller sizes, and parameters now dependent on serial packets.

--- Pacemaker/serial_writing.py
+++ Pacemaker/serial_writing.py
@@ -6,14 +6,11 @@ ser=serial.Serial("COM4",115200, timeout = 2)
 # print("is open: ", ser.is_open)
 # print("serial: ", ser)
 
-# first byte = 22
-# second byte = 34 for echo, 85 for set param
-
 # endian = little
 # defaults to @ which uses native endian
 #   native of windows OS is little
 #   FRDM board set to little endian in model config
-# @ will be first char in format string
+# < will be first char in format string
 
 # byte format string
 # x = pad byte (no value)
@@ -31,16 +28,79 @@ ser=serial.Serial("COM4",115200, timeout = 2)
 # format = 'xxxxxxxx' where x can be any of the above format chars
 
 
-messageS = pack('@HHfH', 152, 2, 3.55, 1)
+# string format:
+#   DCM -> pacemaker:
+#       used to update one of the param in the pacemaker or start sending egram info
+#       [   x   ]   [     x     ]   [  xxxx  ]   [  x  ]
+#       startByte   paramSelector   paramValue   endByte
+#         uint8         uint8         single      uint8
+#       startByte = 34 -> update param
+#       startByte = 85 -> start to send egram info
+#           middle four bytes padding
+#       endByte = 42
+#       format string -> BBfB
+
+#   pacemaker -> DCM:
+#       used to send egram info to DCM or tell DCM whether message received or not
+#       [   x   ]   [ xxxx ]   [  xxxx ]   [  x  ]   [  xxxx  ]   [   x  ]   [  x  ]
+#       startByte     time     atrSignal   atrPace   ventSignal   ventPace   endByte
+#         uint8      single      single     uint8      single       uint8     uint8
+#       startByte = 22 -> send egram info
+#       startByte = 66 -> received message successfully
+#           middle 10 bytes padding
+#       startByte = 101 -> request DCM to resend previous message
+#           middle 10 bytes padding
+#       endByte = 42
+#       format string -> BffBfBB
+
+# Note: when receiving string, check with startByte and endByte
+#       if not valid:
+#           DCM receiver -> have DCM ignore that message from pacemaker
+#           pacemaker receiver -> send request signal to DCM to resend previous message
+
+# Note: explicitly typecast variables into desired type before sending and after receiving
+
+
+# paramSelector:
+#   general
+#   1 -> LRL
+#   2 -> atrium amp
+#   3 -> atrium pulse width
+#   4 -> ventricle amp
+#   5 -> ventricle pulse width
+
+#   inhibit
+#   6 -> hysteresis interval
+#   7 -> RP
+#   8 -> atrium sensitivity
+#   9 -> ventricle sensitivity
+
+#   rate adaptive
+#   10 -> MSR
+#   11 -> activity threshold
+#   12 -> response factor
+#   13 -> reaction time
+#   14 -> recovery time
+#   15 -> rate adaptive on
+
+#   dual pacing
+#   16 -> AV delay
+
+#   operating modes
+#   17 -> sensed
+#   18 -> response
+#   19 -> paced
+
+
+
+messageS = pack('<BBfB', 34, 16, 300, 42)
 print("messageS:     ", messageS)
 print("num bytes: ", ser.write(messageS) )
-#print("num bytes: ", ser.write([22, 85, 0,1,1,1,1,1,1,55]))
 
-messageR_Raw = ser.read(10)
-print("messageR_Raw: ", messageR_Raw)
-messageR = unpack('@HHfH', messageR_Raw);
-print("messageR:     ", messageR);
-#print("unpacked: ", unpack('message', b'\x01\x02\x03\x04\x05\x06\x07\x08\t\n') )
+# messageR_Raw = ser.read(10)
+# print("messageR_Raw: ", messageR_Raw)
+# messageR = unpack('@HHfH', messageR_Raw);
+# print("messageR:     ", messageR);
 # for m in messageR:
 #     print(m, end=' ')
 
